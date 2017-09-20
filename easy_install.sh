4bfa65d600086a65f340584e7811a700cbf154a8
Ramiz A <ramiz.demiurge@gmail.com>
2017-09-20 18:46:31 +0400

    2017 September updates

--- easy_install.sh
+++ easy_install.sh
@@ -2,7 +2,6 @@
 # Easy install
 echo "Allright. Let's go!"
 sudo cp -r Paper-elementary /usr/share/icons/Paper-elementary
-sudo cp -r Paper-Dark-elementary /usr/share/icons/Paper-Dark-elementary
 echo "Setting Paper-elementary."
 gsettings set org.gnome.desktop.interface icon-theme Paper-elementary
 echo "Finish."
